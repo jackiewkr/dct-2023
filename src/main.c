e8cd3cf7c330d6dd92323e9dd88e53ac7f5557b1
Daniel Gatty <106452048+TheGreatGatley@users.noreply.github.com>
2023-01-23 12:42:33 +0000

    Four blinking LEDs
    Commit for four LEDs blinking in sequence

--- src/main.c
+++ src/main.c
@@ -1,103 +1,32 @@
-#include "stm32f4xx_ll_bus.h"
-#include "stm32f4xx_ll_rcc.h"
-#include "stm32f4xx_ll_system.h"
-#include "stm32f4xx_ll_utils.h"
-#include "stm32f4xx_ll_gpio.h"
+#include "stm32f4xx_hal.h"
 
-#define LED_PIN                 LL_GPIO_PIN_5
-#define LED_GPIO_PORT           GPIOA
-#define LED_GPIO_CLK_ENABLE()   LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA)
+void Init_OnBoard_LEDs(void);
+void Delay_ms(volatile int time_ms);
 
-void Configure_GPIO(void)
-{
-  LED_GPIO_CLK_ENABLE();
-  LL_GPIO_SetPinMode(LED_GPIO_PORT, LED_PIN, LL_GPIO_MODE_OUTPUT);
+int main() {
+	Init_OnBoard_LEDs();
+	while (1) {
+        HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_12);
+        Delay_ms(100);
+        HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_13);
+        Delay_ms(100);
+        HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_14);
+        Delay_ms(100);
+        HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_15);
+        Delay_ms(100);
+	}
 }
 
-void SystemClock_Config(void)
-{
-  /* Enable HSE oscillator */
-  LL_RCC_HSE_EnableBypass();
-  LL_RCC_HSE_Enable();
-  while(LL_RCC_HSE_IsReady() != 1)
-  {
-  };
-
-  /* Set FLASH latency */
-  LL_FLASH_SetLatency(LL_FLASH_LATENCY_3);
-
-  /* Main PLL configuration and activation */
-  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_8, 400, LL_RCC_PLLP_DIV_4);
-  LL_RCC_PLL_Enable();
-  while(LL_RCC_PLL_IsReady() != 1)
-  {
-  };
-
-  /* Sysclk activation on the main PLL */
-  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
-  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
-  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
-  {
-  };
-
-  /* Set APB1 & APB2 prescaler */
-  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_2);
-  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
-
-  /* Update CMSIS variable (which can be updated also through SystemCoreClockUpdate function) */
-  SystemCoreClock = SYS_CLOCK;
-  
-  LL_Init1msTick(SystemCoreClock);
+void Init_OnBoard_LEDs() {
+	 __HAL_RCC_GPIOD_CLK_ENABLE();
+	GPIO_InitTypeDef BoardLEDs;
+	BoardLEDs.Mode = GPIO_MODE_OUTPUT_PP;
+	BoardLEDs.Pin = GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
+	HAL_GPIO_Init(GPIOD, &BoardLEDs);
 }
 
-
-
-int main(void)
-{
-  SystemClock_Config();
-  
-  Configure_GPIO();
-  while (1)
-  {
-    LL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
-    LL_mDelay(1000);
-  }
-}
-
-void NMI_Handler(void)
-{
-}
-
-void HardFault_Handler(void)
-{
-  while (1) {}
-}
-
-
-void MemManage_Handler(void)
-{
-  while (1) {}
-}
-
-void BusFault_Handler(void)
-{
-  while (1) {}
-}
-
-void UsageFault_Handler(void)
-{
-  while (1) {}
-}
-
-void SVC_Handler(void)
-{
-}
-
-
-void DebugMon_Handler(void)
-{
-}
-
-void PendSV_Handler(void)
-{
+void Delay_ms(volatile int time_ms) {
+	int j;
+    for (j = 0; j < time_ms*4000; j++)
+        {}  /* excute NOP for 1ms */
 }
